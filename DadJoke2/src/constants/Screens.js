ee926be3feefc9b0511b9b8efb19bcec53ee49e8
Heang Ly <heanglypa@gmail.com>
2022-05-19 08:28:58 +0700

    init

---
+++ DadJoke2/src/constants/Screens.js
@@ -0,0 +1,5 @@
+export default Screens = {
+  home: 'Home',
+  favorite: 'Favorite',
+  notification: 'Notification'
+}
---
+++ DadJoke2/src/screens/NotificationScreen.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native'
+
+const NotificationScreen = () => {
+  return <Text>NotificationScreen</Text>
+}
+
+export default NotificationScreen
